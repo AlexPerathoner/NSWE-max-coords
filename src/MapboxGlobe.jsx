7a86c9add5eaa0cb879db59305cf2162eeabf693
Alex Perathoner <alex.perathoner@gmail.com>
2024-08-24 23:22:08 +0200

    final fixes: formulas of area, logout, custom backend url, alerts

--- src/MapboxGlobe.jsx
+++ src/MapboxGlobe.jsx
@@ -25,8 +25,11 @@ const MapboxGlobe = () => {
     maxSouth: null
   });
   const [username, setUsername] = useState('');
+  const [backendUrl, setBackendUrl] = useState('');
 
   useEffect(() => {
+    setBackendUrl(process.env.REACT_APP_BACKEND_URL);
+    console.debug('backendUrl:', process.env.REACT_APP_BACKEND_URL, backendUrl); // needed. if missing backendUrl will be empty
     // Retrieve username from localStorage
     const storedUsername = localStorage.getItem('username');
     if (!storedUsername) {
@@ -57,6 +60,7 @@ const MapboxGlobe = () => {
       });
 
       mapInstance.on('load', () => {
+        mapInstance.setFog({});
         setMap(mapInstance);
         fetchPointsData(mapInstance, storedUsername); // Pass mapInstance and username to fetchPointsData
       });
@@ -86,32 +90,41 @@ const MapboxGlobe = () => {
       // Clean up on component unmount
       return () => mapInstance.remove();
     }
-  }, []);
+  }, [backendUrl]);
 
   const fetchPointsData = async (mapInstance, user) => {
     if (!mapInstance) return;
-
+    let response;
     try {
-      const response = await fetch(`http://localhost:5002/api/points?user=${user}`);
+      response = await fetch(`${backendUrl}/api/points?user=${user}` , { timeout: 2000 });
+    } catch (error) {
+      alert('Error fetching points data. Please try again later');
+      handleLogout();
+      console.error('Error fetching points data:', error);
+    }
+    try {
       const data = await response.json();
 
       setPoints(data || []);
       updateMapData(mapInstance, data || []);
     } catch (error) {
-      console.error('Error fetching points data:', error);
+      console.log('Error parsing points data (expected, if user has no points (new user)):', error);
     }
   };
 
   const updatePointsDataOnServer = async (updatedPoints, user) => {
     try {
-      await fetch(`http://localhost:5002/api/points?user=${user}`, {
+      await fetch(`${backendUrl}/api/points?user=${user}`, {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
         },
         body: JSON.stringify({ updatedPoints }),
+        timeout: 2000,
       });
     } catch (error) {
+      alert('Error fetching points data. Please try again later');
+      handleLogout();
       console.error('Error updating points data:', error);
     }
   };
@@ -275,8 +288,9 @@ const MapboxGlobe = () => {
       });
     }
     const areaSqDegrees = Math.abs((bounds[1][0] - bounds[0][0]) * (bounds[1][1] - bounds[0][1]));
-    let areaSqKm = (areaSqDegrees * 111.32 * 111.32).toFixed(2);
-    const perc = areaSqDegrees > 0 ? ((areaSqDegrees / 360)).toFixed(2) : 0; // todo fix formula
+    console.debug("steps: ", bounds[1][0], bounds[0][0], bounds[1][1], bounds[0][1], areaSqDegrees);
+    const perc = areaSqDegrees > 0 ? ((areaSqDegrees / (360*180)) * 100).toFixed(2) : 0;
+    let areaSqKm = (areaSqDegrees / (360*180) * 510072000).toFixed(2); // 510072000 is the total area of the earth in km^2, proportional to 360*180 total degrees
     areaSqKm = areaSqKm.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
     setBoundsInfo({
       maxEast: bounds[1][0],
@@ -333,10 +347,10 @@ const MapboxGlobe = () => {
         borderRadius: '5px',
         boxShadow: '0 0 10px rgba(0,0,0,0.3)'
       }}>
-        <div><strong>Max East:</strong> {boundsInfo.maxEast} (Point {boundaryPoints.maxEast})</div>
-        <div><strong>Max West:</strong> {boundsInfo.maxWest} (Point {boundaryPoints.maxWest})</div>
-        <div><strong>Max North:</strong> {boundsInfo.maxNorth} (Point {boundaryPoints.maxNorth})</div>
-        <div><strong>Max South:</strong> {boundsInfo.maxSouth} (Point {boundaryPoints.maxSouth})</div>
+        <div><strong>Max East:</strong> {boundsInfo.maxEast ? boundsInfo.maxEast.toFixed(5) : ""} (Point {boundaryPoints.maxEast})</div>
+        <div><strong>Max West:</strong> {boundsInfo.maxWest ? boundsInfo.maxWest.toFixed(5) : ""} (Point {boundaryPoints.maxWest})</div>
+        <div><strong>Max North:</strong> {boundsInfo.maxNorth ? boundsInfo.maxNorth.toFixed(5) : ""} (Point {boundaryPoints.maxNorth})</div>
+        <div><strong>Max South:</strong> {boundsInfo.maxSouth ? boundsInfo.maxSouth.toFixed(5) : ""} (Point {boundaryPoints.maxSouth})</div>
         <div><strong>Area:</strong> {boundsInfo.area} km^2 ({boundsInfo.percOfTotal}%)</div>
       </div>
       <div 
@@ -350,7 +364,11 @@ const MapboxGlobe = () => {
         }}
         onClick={handleLogout}
       >
-        &#10005; {/* Cross icon for logout */}
+        <div style={{ transform: 'scale(2)', padding: '10px' }}>
+        <svg xmlns="http://www.w3.org/2000/svg" enable-background="new 0 0 24 24" height="24" viewBox="0 0 24 24" width="24" focusable="false" aria-hidden="true" fill="#fff">
+          <path d="M20 3v18H8v-1h11V4H8V3h12zm-8.9 12.1.7.7 4.4-4.4L11.8 7l-.7.7 3.1 3.1H3v1h11.3l-3.2 3.3z"></path>
+        </svg>
+        </div>
       </div>
       <CustomModal
         isOpen={modalIsOpen}
